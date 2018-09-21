de36137e6dd4c2018364bacea5f860b15a8a8109
fgth <fsoyer@systea.fr>
2018-09-21 16:17:18 +0200

    format README

--- audit_mysql_html.sql
+++ audit_mysql_html.sql
@@ -1,7 +1,20 @@
 -- AUDIT BASES MYSQL
 -- v1.4
 -- Compatible MySQL 5.0.6 minimum (information_schema.global_status), MariaDB 10
--- FSo 2013-2018
+-- (c) 2013, Frank Soyer <frank.soyer@gmail.com>
+
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- The GNU General Public License is available at:
+-- http://www.gnu.org/copyleft/gpl.html
 
 -- ================================================= SCRIPT D'AUDIT =========================================
 
